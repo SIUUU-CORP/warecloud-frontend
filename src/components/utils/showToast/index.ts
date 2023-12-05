08d023ab7bc925f0e5cb3d0bc811f6661535bb05
Fadhlan Hasyim <81791118+fadhlanhasyim@users.noreply.github.com>
2023-12-06 01:19:17 +0700

    fix: session expired (#8)

--- src/components/utils/showToast/index.ts
+++ src/components/utils/showToast/index.ts
@@ -1,9 +1,15 @@
 import { ShowToastInterface } from './interface'
 
-export const showToast = ({ status, title, toast }: ShowToastInterface) => {
+export const showToast = ({
+  status,
+  title,
+  toast,
+  description,
+}: ShowToastInterface) => {
   toast({
     status,
     title,
+    description,
     duration: 5000,
     isClosable: true,
     position: 'top',
