2723f359eb92241f77a8fd396a608e80839ffed0
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-07 01:51:01 +0700

    prettier

--- src/components/ssr/getItemProps/index.ts
+++ src/components/ssr/getItemProps/index.ts
@@ -4,14 +4,16 @@ import type { GetServerSideProps } from 'next'
 export const getItemProps: GetServerSideProps = async (context) => {
   const itemId = context.params?.itemId
 
-  const { data: {item} } = await axios({
+  const {
+    data: { item },
+  } = await axios({
     method: 'GET',
     url: `${process.env.NEXT_PUBLIC_APP_API_URL}/item/${itemId}`,
   })
 
   return {
     props: {
-      item
+      item,
     },
   }
 }
--- src/pages/[vendorName]/[itemId]/index.tsx
+++ src/pages/[vendorName]/[itemId]/index.tsx
@@ -4,10 +4,12 @@ import type { GetServerSideProps, NextPage } from 'next'
 import { getItemProps } from '@ssr'
 import { DetailItemModuleProps } from 'src/components/modules/DetailItemModule/interface'
 
-const DetailItem: NextPage<DetailItemModuleProps> = ({ item }) => <DetailItemModule item={item} />
+const DetailItem: NextPage<DetailItemModuleProps> = ({ item }) => (
+  <DetailItemModule item={item} />
+)
 
 export default DetailItem
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    return await getItemProps(context)
+  return await getItemProps(context)
 }
